73a52351f9ceedfb8b2fdd9fcb1ccd766c0797e6
Authoi234 <jawadkhan.authoi@gmail.com>
2025-02-21 00:22:21 +0600

    email and password fields are in progress

---
+++ src/pages/studentPortal/Login.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import learningPortalImg from "../../assets/image/learningportal.svg";
+import useTitle from './../../hooks/useTitle';
+import { Link, useNavigate } from 'react-router-dom';
+import { useLoginMutation } from '../../features/auth/authApi';
+
+const Login = () => {
+    useTitle("Student Login");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
+    const [login, { data, isLoading, error: responseError }] = useLoginMutation();
+
+    useEffect(() => {
+        if (responseError?.data) {
+            setError(responseError.data);
+        }
+        if (data?.accessToken && data?.user) {
+            navigate("/course");
+        }
+    }, [data, responseError, navigate]);
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setError("");
+
+        login({email, password});
+    };
+
+    return (
+        <section className="py-6 bg-primary h-screen grid place-items-center" onSubmit={handleSubmit}>
+            <div className="mx-auto max-w-md px-5 lg:px-0">
+                <div>
+                    <img className="h-12 mx-auto" src={learningPortalImg} alt='' />
+                    <h2 className="mt-6 text-center text-3xl font-extrabold text-slate-100">
+                        Sign in to Student Account
+                    </h2>
+                </div>
+                <form className="mt-8 space-y-6" action="#" method="POST">
+                    <input type="hidden" name="remember" value="true" />
+                    <div className="rounded-md shadow-sm -space-y-px">
+                        <div>
+                            <label for="email-address" className="sr-only">Email address</label>
+                            <input id="email-address" name="email" type="email" autocomplete="email" required
+                                className="login-input rounded-t-md" placeholder="Email address" value={email} onChange={e => setEmail(e.target.value)} />
+                        </div>
+                        <div>
+                            <label for="password" className="sr-only">Password</label>
+                            <input id="password" name="password" type="password" autocomplete="current-password" required value={password} onChange={e => setPassword(e.target.value)} className="login-input rounded-b-md" placeholder="Password" />
+                        </div>
+                    </div>
+
+                    <div className="flex items-center justify-end">
+                        <div className="text-sm">
+                            <Link to="./registration" className="font-medium text-violet-600 hover:text-violet-500">
+                                Create New Account
+                            </Link>
+                        </div>
+                    </div>
+
+                    <div>
+                        <button type="submit"
+                            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500">
+                            Sign in
+                        </button>
+                    </div>
+                    {error !== "" && <div style={{ color: "white", fontWeight: "bold", fontSize: "15px", padding: "8px ", margin: "10px 0", borderRadius: "5px", backgroundColor: "#fc4242" }}>There was an error Occured</div>}
+                </form>
+            </div>
+        </section>
+    );
+};
+
+export default Login;
---
+++ src/pages/studentPortal/Registration.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from 'react';
+import learningPortalImg from "../../assets/image/learningportal.svg";
+import useTitle from '../../hooks/useTitle';
+import { useNavigate } from 'react-router-dom';
+import { useRegisterMutation } from '../../features/auth/authApi';
+
+const Registration = () => {
+    useTitle("Student Registration");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
+    const [register, { data, isLoading, error: responseError }] = useRegisterMutation();
+
+    useEffect(() => {
+        if (responseError?.data) {
+            setError(responseError.data);
+        }
+        if (data?.accessToken && data?.user) {
+            navigate("/course");
+        }
+    }, [data, responseError, navigate]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setError("");
+        if (password !== confirmPassword) {
+            setError("Password Didnt Match")
+        }
+        else {
+            register({name, email, password, role: "student"});
+        }
+    };
+
+    return (
+        <section onSubmit={handleSubmit} className="py-6 bg-primary h-screen grid place-items-center">
+            <div className="mx-auto max-w-md px-5 lg:px-0">
+                <div>
+                    <img className="h-12 mx-auto" src={learningPortalImg} alt="" />
+                    <h2 className="mt-6 text-center text-3xl font-extrabold text-slate-100">
+                        Create Your New Account
+                    </h2>
+                </div>
+                <form className="mt-8 space-y-6" action="#" method="POST">
+                    <input type="hidden" name="remember" value="true" />
+                    <div className="rounded-md shadow-sm -space-y-px">
+                        <div>
+                            <label for="name" className="sr-only">Name</label>
+                            <input id="name" name="name" type="name" autocomplete="name" required
+                                className="login-input rounded-t-md" placeholder="Student Name" value={name} onChange={e => setName(e.target.value)} />
+                        </div>
+                        <div>
+                            <label for="email-address" className="sr-only">Email address</label>
+                            <input id="email-address" name="email" type="email" autocomplete="email" required value={email} onChange={e => setEmail(e.target.value)}
+                                className="login-input " placeholder="Email address" />
+                        </div>
+                        <div>
+                            <label for="password" className="sr-only">Password</label>
+                            <input id="password" name="password" type="password" autocomplete="current-password" required value={password} onChange={e => setPassword(e.target.value)}
+                                className="login-input" placeholder="Password" />
+                        </div>
+                        <div>
+                            <label for="confirm-password" className="sr-only">Confirm Password</label>
+                            <input id="confirm-password" name="confirm-password" type="password"
+                                autocomplete="confirm-password" required className="login-input rounded-b-md" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)}
+                                placeholder="Confirm Password" />
+                        </div>
+                    </div>
+
+                    <div>
+                        <button type="submit"
+                            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500">
+                            Create Account
+                        </button>
+                    </div>
+                    {error !== "" && <div style={{ color: "white", fontWeight: "bold", fontSize: "15px", padding: "8px ", margin: "10px 0", borderRadius: "5px", backgroundColor: "#fc4242" }}>{error}</div>}
+                </form>
+            </div>
+        </section>
+    );
+};
+
+export default Registration;
